9d27daa62f6ea4aa72180e5ae5497b4ac482ba45
quinnium <78171763+quinnium@users.noreply.github.com>
2024-06-07 10:28:18 +0100

    Initial Commit

---
+++ PokemonLookup/PokemonLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonLookupApp.swift
+//  PokemonLookup
+//
+//  Created by Quinn on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
