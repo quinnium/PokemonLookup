9019d2b8cc9e38ef6527cb440599ca5b00a2fb6d
quinnium <78171763+quinnium@users.noreply.github.com>
2024-06-16 10:48:55 +0100

    Amended PLBrandedTextView to instead create 'border' using backgrounds (as opposed to using  shadows), as using compounded shadows was causing crashes when changing orientation on real devices

--- PokemonLookup/Views/Shared Views/PLBrandedTextView.swift
+++ PokemonLookup/Views/Shared Views/PLBrandedTextView.swift
@@ -12,23 +12,44 @@ struct PLBrandedTextView: View {
     let size: CGFloat
     var color: Color = .yellow
     var border: Color = .clear
+    private var borderOffset: CGFloat {
+        size / 16
+    }
     
     var body: some View {
-        Text(text)
-            .foregroundStyle(color)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .shadow(color: .blue, radius: size/80)
-            .font(.custom("Pokemon Solid", size: size))
+        
+        HStack(spacing: -0.93 * size) {
+            ForEach(Array(text.enumerated()), id: \.offset) { stringElement in
+                // Provide spaces to avoid custom font being clipped
+                let char = " " + String(stringElement.element) + " "
+                
+                ZStack {
+                    // Provide 'border' by giving 8 backgrounds, offset in 8 different directions
+                    ZStack {
+                        Text(char).offset(x: 0, y: -borderOffset)
+                        Text(char).offset(x: borderOffset, y: -borderOffset)
+                        Text(char).offset(x: borderOffset, y: 0)
+                        Text(char).offset(x: borderOffset, y: borderOffset)
+                        Text(char).offset(x: 0, y: borderOffset)
+                        Text(char).offset(x: -borderOffset, y: borderOffset)
+                        Text(char).offset(x: -borderOffset, y: 0)
+                        Text(char).offset(x: -borderOffset, y: -borderOffset)
+                    }
+                    .foregroundStyle(border)
+                    Text(char)
+                        .foregroundStyle(color)
+                }
+                .zIndex(Double(-stringElement.offset))
+                .font(.custom("Pokemon Solid", size: size))
+                // Disable dynamic text sizing, as not wanted for this review
+                .environment(\.dynamicTypeSize, .medium)
+            }
+        }
+        // Vertical padding required here to avoid clipping of any underhanging letters due to this custom font
+        .padding(.vertical)
     }
 }
 
 #Preview {
-    PLBrandedTextView(text: "Pokemon \n Lookup", size: 80, color: .yellow, border: .blue)
+    PLBrandedTextView(text: "PokemonSaurus", size: 40, color: .yellow, border: .blue)
 }
